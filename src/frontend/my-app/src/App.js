3beb5ee792bf88fc3cb97a063d52329648dbf20d
Emma Smith <emmasmith325@icloud.com>
2024-09-15 14:46:40 -0400

    Set up VERY basic react app structure

---
+++ src/frontend/my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Header for our application!
+        </p>
+      </header>
+      <body className = "App-body">
+        <p>
+          Body for our application will go here!
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
